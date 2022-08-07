4fd163dcfc045a6e2678edab69d45e1c7cbb3d7a
NSLyuma <iinokayo@mail.ru>
2022-08-07 16:36:34 +0300

    Creating uploads folder

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import fs from "fs";
 import mongoose from "mongoose";
 import {
   registerValidation,
@@ -20,6 +21,9 @@ const app = express();
 //хранилище для хранения картинок
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
+    if (!fs.existsSync("uploads")) {
+      fs.mkdirSync("uploads");
+    }
     cb(null, "uploads");
   },
   filename: (_, file, cb) => {
