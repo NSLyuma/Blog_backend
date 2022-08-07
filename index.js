82eb8147018584dc5f956721f1ea3ebeae2e0b30
NSLyuma <iinokayo@mail.ru>
2022-08-07 17:47:55 +0300

    Nothing

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 import express from "express";
-// import fs from "fs";
-import multer from "multer";
-import cors from "cors";
 import mongoose from "mongoose";
 import {
   registerValidation,
   loginValidation,
   postCreateValidation,
 } from "./validations.js";
-import { handleValidationErrors, checkAuth } from "./utils/index.js";
 import { UserController, PostController } from "./controllers/index.js";
+import multer from "multer";
+import { handleValidationErrors, checkAuth } from "./utils/index.js";
+import cors from "cors";
 
-//"mongodb+srv://admin:wwwww@cluster0.dwpow.mongodb.net/blog?retryWrites=true&w=majority"
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(
+    "mongodb+srv://admin:wwwww@cluster0.dwpow.mongodb.net/blog?retryWrites=true&w=majority"
+  )
   .then(() => console.log("DB OK"))
   .catch((err) => console.log("DB error", err));
 
@@ -22,9 +22,6 @@ const app = express();
 //хранилище для хранения картинок
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
-    // if (!fs.existsSync("uploads")) {
-    //   fs.mkdirSync("uploads");
-    // }
     cb(null, "uploads");
   },
   filename: (_, file, cb) => {
@@ -88,7 +85,7 @@ app.patch(
 );
 
 //на какой порт прикрепить наше приложение (тут 4444)
-app.listen(process.env.PORT || 4444, (err) => {
+app.listen(4444, (err) => {
   if (err) {
     //если сервер не смог запуститься, то мы вернём сообщение об этом (ошибка)
     return console.log(err);
